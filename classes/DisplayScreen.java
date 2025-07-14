78281f63e1c59c2073bb7cafe23c9e4ec3bfe97a
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-07-14 17:39:24 -0300

    feat: file renamed

--- classes/DisplayScreen.java
+++ classes/DisplayScreen.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 import javax.swing.*;
 //import java.awt.*;
 
-public class ExhibitionScreen extends JFrame{
+public class DisplayScreen extends JFrame{
     private ArrayList<Resource> resourceList;
 }
