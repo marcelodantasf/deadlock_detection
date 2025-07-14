a6b5ea7a239d9ee5d8c2dbfe6f9722808e863b57
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-07-14 15:02:53 -0300

    feat: SO implemented

--- classes/SO.java
+++ classes/SO.java
@@ -1,3 +1,39 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class SO {
-    
+    private int deltaT;
+
+    public SO(int deltaT){
+        this.deltaT = deltaT;
+    }
+
+    public int getDeltaT() {
+        return this.deltaT;
+    }
+
+    public void waiting(){
+        int time = this.deltaT;
+        LocalTime initial = LocalTime.now();
+        while (true) { // função de espera por X segundos
+            LocalTime now = LocalTime.now();
+            Duration duration = Duration.between(initial, now);
+            float length = duration.toMillis() / 1000f;
+
+            if(length >= (float) time){
+                break;
+            }
+        }
+    }
+
+    public void check(){
+
+    }
+
+    public void run(){
+        while(true){
+            waiting();
+            check();
+        }
+    }
 }
