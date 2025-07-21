a33794a9516480d4b3ca934e98b6d38103bff6ec
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-07-21 15:35:53 -0300

    feat: map list of allocated and requested resources in process

--- classes/Process.java
+++ classes/Process.java
@@ -3,7 +3,9 @@ Podem existir até 10 processos rodando “simultaneamente”.*/
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Process extends Thread{
@@ -14,6 +16,9 @@ public class Process extends Thread{
     private int intervalRequisition; //em segundos
     private int intervalUsage; //em segundos
     private volatile boolean running = true;
+
+    private Map<Integer, Integer> allocated = new HashMap<>();
+    private Map<Integer, Integer> requested = new HashMap<>();
 
     private Random random = new Random();
 
@@ -83,11 +88,26 @@ public class Process extends Thread{
         }
     }
 
+    public int getAllocated(int resourceIndex) {
+        return allocated.getOrDefault(resourceIndex, 0);
+    }
+
+    public int getRequested(int resourceIndex) {
+        return requested.getOrDefault(resourceIndex, 0);
+    }
+
     public void requestResource(){
         Resource resourceSelected = selectResource();
+        int index = resourceList.indexOf(resourceSelected);
+
         System.out.println("Processo " + this.id + " requisitou recurso " + resourceSelected.getName() + " .");
         
+        requested.put(index, requested.getOrDefault(index, 0) + 1);
+        
         resourceSelected.acquireResource();
+
+        allocated.put(index, allocated.getOrDefault(index, 0) + 1);
+
         ProcessThread thread = new ProcessThread(resourceSelected, this.intervalUsage);
         thread.start();
     }
